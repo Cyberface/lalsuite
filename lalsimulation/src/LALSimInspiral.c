2a878dd927907e73e4e351f10969eef8cb998a1c
Sebastian Khan <Sebastian.Khan@LIGO.org>
2017-06-08 16:29:52 +0200

    add PhenomHM to list of TD approx

--- lalsimulation/src/LALSimInspiral.c
+++ lalsimulation/src/LALSimInspiral.c
@@ -720,6 +720,21 @@ int XLALSimInspiralChooseTDWaveform(
 	    XLALGPSSetREAL8(&(hc->epoch), (-1.) * deltaT * maxind);
 	    break;
 
+	case IMRPhenomHM:
+        /* Waveform-specific sanity checks */
+		if( !XLALSimInspiralWaveformFlagsIsDefault(waveFlags) )
+            ABORT_NONDEFAULT_WAVEFORM_FLAGS(waveFlags);
+        if( !checkTransverseSpinsZero(S1x, S1y, S2x, S2y) )
+            ABORT_NONZERO_TRANSVERSE_SPINS(waveFlags);
+        if( !checkTidesZero(lambda1, lambda2) )
+            ABORT_NONZERO_TIDES(waveFlags);
+        /* Call the waveform driver routine */
+		ret = XLALSimInspiralTDFromFD(hplus, hcross, phiRef, deltaT, m1, m2, S1x, S1y, S1z,
+			    S2x, S2y, S2z, f_min, f_ref, r, 0, 0, lambda1, lambda2,
+			    waveFlags, nonGRparams, amplitudeO, phaseO, approximant);
+        if (ret == XLAL_FAILURE) XLAL_ERROR(XLAL_EFUNC);
+        break;
+
 	case IMRPhenomPv2:
 	    ret = XLALSimInspiralTDFromFD(hplus, hcross, phiRef, deltaT, m1, m2, S1x, S1y, S1z,
 			    S2x, S2y, S2z, f_min, f_ref, r, 0, i, lambda1, lambda2,
@@ -3980,6 +3995,7 @@ int XLALSimInspiralImplementedTDApproximants(
         case PhenSpinTaylor:
         case IMRPhenomC:
 	case IMRPhenomD:
+	case IMRPhenomHM:
 	case IMRPhenomPv2:
         case PhenSpinTaylorRD:
         case SEOBNRv1:
